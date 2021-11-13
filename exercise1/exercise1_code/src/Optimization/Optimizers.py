d5c3f25f4f7c000dcac95b8349b36011a528b368
um59ipuh <arup.sarker@fau.de>
2021-11-13 17:19:17 +0100

    add Sgd class [skeleton]

---
+++ exercise1/exercise1_code/src/Optimization/Optimizers.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Sgd:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        # TODO: implementation of sgd
+        pass
